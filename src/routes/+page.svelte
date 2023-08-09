18843e2e226b276d374a354025dff70ee9a16209
BaukeWestendorp <baukewestendorp@gmail.com>
2023-08-09 16:33:25 +0200

    Add 'Gedichtenbundel Nr.3' bundle

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -2,4 +2,5 @@
 <ul>
 	<li><a href="/bundels/dwangschematiek">Onder het mom van Dwangschematiek</a></li>
 	<li><a href="/bundels/facetten-der-ouderdom">De Facetten der Ouderdom</a></li>
+	<li><a href="/bundels/gedichtenbundel-nr3">Gedichtenbundel Nr.3</a></li>
 </ul>
---
+++ src/routes/bundels/gedichtenbundel-nr3/+page.svelte
@@ -0,0 +1,490 @@
+<h2>Gedichtenbundel Nr.3</h2>
+<p><i>15 gedichten in moedertaal</i></p>
+
+<section>
+	<h3>Heeft u liever koffie of thee?</h3>
+	<p>
+		Ga toch zitten, ga toch zitten, zegt de meneer tegen de klant
+		<br />
+		U moet zich om zulke zaken echt niet zo druk maken toch
+		<br />
+		Het is ook allemaal niet zo erg als ze op het nieuws nu beweren
+		<br />
+		Als uw vrouw echt van u houdt zal het allemaal wel goedkomen of niet
+		<br />
+		Uiteindelijk komt immers alles toch uiteindelijk alles toch wel weer goed
+		<br />
+		Nou nou, ho ho, toe toe, nou meneer toe nou eens even
+		<br />
+		Als u zich zo blijft verzetten dan zijn wij toch echt genoodzaakt
+		<br />
+		De autoriteiten
+		<br />
+		Wij waarborgen hier nu op dit moment een bepaalt niveau van integriteit
+		<br />
+		en u schaadt dit met de prominente aanwezigheid van uw zorgen en klachten
+		<br />
+		Mijn bekwaamheid kent zo zijn grenzen en anders ben ik genoodzaakt
+		<br />
+		Bewust de mogelijkheid geen andere oplossingen te zien
+		<br />
+		Bewust van een ander genootschap gewetenloos zeer
+	</p>
+	<p>MENEER</p>
+	<p>
+		Kijkt u mij toch eens aan terwijl ik tegen u praat godverdomme
+		<br />
+		Uw dekens kunnen zo over u heen getrokken worden
+		<br />
+		Afgetrokken worden
+		<br />
+		Elk moment kunnen inslapen
+		<br />
+		Met een klein spuitje echt het doet echt geen zeer
+		<br />
+		Meneer
+	</p>
+	<p>
+		Meneer
+		<br />
+		Meneer
+		<br />
+		Heeft u liever koffie of thee
+		<br />
+		Meneer
+	</p>
+	<p>Meneer?</p>
+</section>
+
+<section>
+	<h3>Knapperig</h3>
+	<p>
+		Ik bijt in een Cornetto® aan de verkeerde kant omdat ik het even niet begrepen had en het
+		allemaal even niet meer zag zitten
+	</p>
+</section>
+
+<section>
+	<h3>Lijstje</h3>
+	<p>
+		Kledinghangers
+		<br />
+		Paprika's
+		<br />
+		Een grote zak met limoenen erin
+		<br />
+		Een half tijgerbrood
+		<br />
+		Een gram zaadjes van een plant
+		<br />
+		Tweeëntwintig kilo paardenvlees
+		<br />
+		Van die kleine velletjes aan de onderkant van je nagel
+		<br />
+		Waar als je er aan trekt dat je dan denkt van
+		<br />
+		Een roodharige man met okselhaar dat ook rood is
+	</p>
+	<p>Kledinghangers</p>
+	<p>
+		Een fles koolzuurhoudend water
+		<br />
+		Een klein potje zwarte olijven of groene maakt echt niet uit
+		<br />
+		Een tuin op het Zuiden of in het Noorden
+		<br />
+		Niet bakpapier maar dat andere
+		<br />
+		3 pimentkorrels
+		<br />
+		Een foto van Sinterklaas zijn navelstreng
+		<br />
+		Een antwoord op mijn vraag over
+	</p>
+	<p>Kledinghangers</p>
+	<p>En van die vieze boze witte mannen met stinkende haren</p>
+</section>
+
+<section>
+	<h3>Dagdromen</h3>
+	<p>
+		Ik durf het er eigenlijk niet over te hebben
+		<br />
+		Dus laat ik het dan ook maar niet doen
+	</p>
+	<p>
+		En vaak wil ik ook niet eens dat je het weet
+		<br />
+		Maar soms toch ook wel
+		<br />
+		En dan zal ik het je heus wel vertellen hoor
+	</p>
+	<p>Maar meestal toch liever niet</p>
+	<p>
+		Daarom ben ik wel blij dat andere mensen
+		<br />
+		Niet kunnen zien waar ik aan denk
+	</p>
+	<p>
+		En ik denk dat zij dat ook denken
+		<br />
+		Durf ik te wedden
+	</p>
+</section>
+
+<section>
+	<h3>Een vrouw zit op haar fiets</h3>
+	<p>
+		Een vrouw zit op haar fiets op weg naar haar werk als opeens
+		<br />
+		Het licht uitvalt
+		<br />
+		Ook met ogen valt even niks te zien
+		<br />
+		Van schrik knijpt ze hard in haar remmen
+		<br />
+		En zet een voet op de grond
+		<br />
+		Maar de grond is ook verdwenen
+		<br />
+		En ze blijft maar vallen en vallen
+		<br />
+		En vallen
+	</p>
+	<p>Tot opeens het licht weer aangaat</p>
+	<p>
+		Maar ze heeft helemaal geen zonnebril op
+		<br />
+		Waar is haar zonnebril gebleven ze had hem net nog
+		<br />
+		Zonder zonnebril kan ze toch niks zien
+		<br />
+		Snel kijkt ze naar haar handen
+		<br />
+		Als ze geen handen heeft dan droomt ze
+		<br />
+		Maar ze ziet de voegen van haar hand
+		<br />
+		Op een van de voegen loopt een juf
+		<br />
+		Ze zegt
+		<br />
+		“Ik ben een juf en het komt wel weer goed
+		<br />
+		Het licht wordt straks wel weer zichtbaar”
+	</p>
+	<p>Gelukkig hebben juffen altijd gelijk</p>
+	<p>En vol opluchting vervolgt de vrouw haar weg</p>
+</section>
+
+<section>
+	<h3>Filosofobie</h3>
+	<p>~De angst voor filosofen</p>
+</section>
+
+<section>
+	<h3>Utopie</h3>
+	<p>Konden we maar altijd gewoon even lekker gaan slapen</p>
+	<p>
+		En dat alle huizen van die poepfonteintjes hebben voor nadat je
+		<br />
+		van de wc komt even je poep van je billen af kunt wassen zonder
+		<br />
+		Een rauw anusgaatje te krijgen met dat geveeg in de winter
+	</p>
+	<p>Dat lijkt me erg fijn</p>
+</section>
+
+<section>
+	<h3>Tegenwoordigheid</h3>
+	<p>
+		Is iets ooit precies
+		<br />
+		Op de helft
+	</p>
+	<p>Of in het nu</p>
+	<p>
+		Want het voelt altijd net alsof iets altijd nét
+		<br />
+		Voorbij is als
+	</p>
+	<p>Je er aan denkt</p>
+</section>
+
+<section>
+	<h3>Van Zintuigen</h3>
+	<p>
+		De geur die je soms ruikt
+		<br />
+		Die je vertelt dat het nieuwe seizoen
+		<br />
+		Al begonnen is
+		<br />
+		Zonder het je te vertellen
+	</p>
+	<p>
+		De wolk die precies
+		<br />
+		Vlak voor de zon
+		<br />
+		Glijdt zodat
+		<br />
+		Jij weer kunt zien
+	</p>
+	<p>
+		Het gevoel dat als je
+		<br />
+		Je herinnert
+		<br />
+		Die ene keer
+		<br />
+		Dat je moeder
+		<br />
+		Dat ene tegen je zei
+		<br />
+		Waardoor je het gevoel kreeg
+		<br />
+		Je haar
+	</p>
+	<p>
+		Je gewoon proeft
+		<br />
+		Dat hij
+		<br />
+		Nog niet gedoucht heeft vandaag
+	</p>
+</section>
+
+<section>
+	<h3>Dommelen</h3>
+	<p>Kun je je het nog herinneren</p>
+	<p>Drie seconden voordat je in sla-</p>
+</section>
+
+<section>
+	<h3>Een Twee Drie</h3>
+	<p>
+		123
+		<br />
+		1234
+		<br />
+		12345
+	</p>
+	<p>
+		Zes Zeven Acht Negen
+		<br />
+		Tien
+	</p>
+	<p>
+		Elf Twaalf
+		<br />
+		Dertien
+		<br />
+		Veertien
+	</p>
+	<p>
+		Kut
+		<br />
+		Wacht
+	</p>
+	<p>
+		Oh
+		<br />
+		Oh nee ging toch wel goed
+	</p>
+	<p>15</p>
+	<p>Wacht</p>
+	<p>Nee ja wel echt vijftien</p>
+	<p>Toch</p>
+	<p>Wacht</p>
+</section>
+
+<section>
+	<h3>Ik heb opeens een geniale ingeving</h3>
+	<p>
+		Inspiratie is geen emmer water
+		<br />
+		Die opeens uit de lucht komt vallen
+		<br />
+		Geen oase die je vindt in een woestijn
+	</p>
+	<p>
+		Inspiratie is het water
+		<br />
+		Dat in zo'n schoonmaakdoekje zit
+		<br />
+		En je met veel kracht er
+		<br />
+		Uit moet wringen
+		<br />
+		Elke dag opnieuw
+		<br />
+		Tot de emmer vol zit
+	</p>
+	<p>
+		En dan gooi je hem recht
+		<br />
+		Over iemands dikke hoofd
+	</p>
+	<p>Inspiratie bestaat niet meer!</p>
+	<p>
+		Behalve onder de douch
+		<br />
+		Of in een achtbaa
+		<br />
+		Of waar je ook maar net niet iets bij de hand had
+	</p>
+</section>
+
+<section>
+	<h3>Just that little eternal moment</h3>
+	<p>
+		Time moves slower in the eternal aether
+		<br />
+		Everything moves slower there
+		<br />
+		From the little grains under the grass
+		<br />
+		To the flow of water and the movement
+		<br />
+		of the great tectonic plates
+	</p>
+	<p>
+		At the first grasp of the clouds of Artaur
+		<br />
+		Past the third outer ring of Goliath
+		<br />
+		Where no earthquakes have ever been recorded
+	</p>
+	<p>
+		There I saw him
+		<br />
+		Just for a quick moment
+	</p>
+	<p>
+		I don’t think he saw me
+		<br />
+		My movements were too quick
+		<br />
+		I shouldn’t have gone back
+		<br />
+		He would see me move
+		<br />
+		Age and then die
+		<br />
+		But still I went back
+	</p>
+	<p>
+		He had barely moved
+		<br />
+		I wish I could speak to him
+		<br />
+		But he wouldn’t understand
+		<br />
+		I wish I could just take him home
+		<br />
+		Incapsulate him in glass
+		<br />
+		And just look at him just look
+		<br />
+		But that would be just useless
+	</p>
+	<p>
+		Yet still I thought of it
+		<br />
+		I could stare at him for eternity
+		<br />
+		I could stare at him my whole life
+		<br />
+		I could even touch him
+		<br />
+		But I could never tell how he feels
+		<br />
+		He could hate me, and I would be dead
+		<br />
+		And if he liked me, I would quickly disappear in time
+	</p>
+	<p>
+		It has no use
+		<br />
+		I need to leave
+		<br />
+		He will barely even have noticed
+		<br />
+		I existed
+	</p>
+	<p>
+		And then I will remember him
+		<br />
+		But I must admit
+		<br />
+		I did take his jacket
+		<br />
+		It smells nice
+		<br />
+		He will soon notice it has gone
+		<br />
+		But I don’t think he noticed me
+	</p>
+	<p>
+		And if he did
+		<br />
+		What would he have thought
+	</p>
+</section>
+
+<section>
+	<h3>Huh</h3>
+	<p>
+		Huh
+		<br />
+		Hallo
+		<br />
+		Ja?
+		<br />
+		Hallo?
+	</p>
+	<p>Oh</p>
+	<p>
+		Ja
+		<br />
+		Ja met mij ja
+	</p>
+	<p>
+		Nee
+		<br />
+		Ja
+		<br />
+		Ja
+		<br />
+		Ok
+		<br />
+		Ja
+		<br />
+		Ok
+		<br />
+		Ja is goed
+		<br />
+		Ok
+		<br />
+		Ja, ja, ja, ja
+	</p>
+	<p>Nee</p>
+	<p>Nee, nee</p>
+	<p>Ja</p>
+	<p>
+		Yes
+		<br />
+		Ok is goed doen we!
+	</p>
+	<p>Ja!</p>
+	<p>Joejoe</p>
+	<p>Joe, hoihoi</p>
+	<p>Hoi</p>
+</section>
+
+<section>
+	<h3>Soms begin je aan iets</h3>
+	<p>Soms begin je aan iets</p>
+	<p>en maak je het niet</p>
+</section>
